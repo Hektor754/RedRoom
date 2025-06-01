27e0f23a533ff5e74e345a0df8cf3d1a0f67b28a
ektoras <ekal2004@gmail.com>
2025-06-01 18:17:16 +0300

    added timeout,retries logic

--- categories/recon/methods/arp_scan.py
+++ categories/recon/methods/arp_scan.py
@@ -3,6 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 from ipaddress import ip_network
 from termcolor import colored
 from colorama import init
+import time
 
 init()
 
@@ -20,7 +21,7 @@ def print_arp_result(ip, status):
     }
     print(f"{ip:<20}{colored(status, status_colors.get(status, 'white'))}")
 
-def arp_scan(target_ip, max_workers=50):
+def arp_scan(target_ip, timeout, retries, max_workers=50):
     try:
         network = ip_network(target_ip, strict=False)
     except ValueError:
@@ -30,7 +31,7 @@ def arp_scan(target_ip, max_workers=50):
     print_arp_banner()
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        results = executor.map(arp_request_ip, network.hosts())
+        results = executor.map(lambda ip: arp_request_ip(ip,timeout,retries), network.hosts())
 
     active_ips = []
     for ip, active in results:
@@ -42,11 +43,14 @@ def arp_scan(target_ip, max_workers=50):
 
     return active_ips
 
-def arp_request_ip(ip):
+def arp_request_ip(ip,timeout,retries):
     arp_req = ARP(pdst=str(ip))
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether / arp_req
-    ans, _ = srp(packet, timeout=1, verbose=False)
-    for sent, received in ans:
-        return (str(ip), True)
+
+    for attempt in range(retries):
+        ans, _ = srp(packet, timeout=timeout, verbose=False)
+        if ans:
+            return (str(ip), True)
+        time.sleep(0.1)
     return (str(ip), False)
