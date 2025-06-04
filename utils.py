54962d8904970fe43264d43ea9788630bdd90686
ektoras <ekal2004@gmail.com>
2025-06-04 21:23:26 +0300

    added hostname def

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import csv
 import json
 import os
+import socket
+import ipaddress
+import ifaddr
+import subprocess
 
 
 def print_summary(results,scantype):
@@ -15,7 +19,7 @@ def print_summary(results,scantype):
 
 def save_results_csv(results, filename):
     with open(filename, mode="w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=["ip", "status"])
+        writer = csv.DictWriter(f, fieldnames=["hostname", "ip", "status"])
         writer.writeheader()
         writer.writerows(results)
     print(f"\n[+] results saved to {filename}")
@@ -45,3 +49,36 @@ def handle_scan_output(results, scantype, filename=None, ftype=None):
             save_results_csv(results, filename)
         elif ftype == "json":
             save_results_json(results, filename)
+
+def resolve_hostname(target_ip):
+        target_addr = ipaddress.IPv4Address(target_ip)
+        adapters = ifaddr.get_adapters()
+
+        for adapter in adapters:
+            for ip in adapter.ips:
+                if isinstance(ip.ip, tuple):
+                    continue
+
+                local_ip = ip.ip
+                network = ipaddress.IPv4Network(f"{local_ip}/{ip.network_prefix}", strict=False)
+
+                if target_addr in network:
+                    try:
+                        output = subprocess.check_output(
+                            ['nmblookup', '-A', target_ip],
+                            stderr=subprocess.DEVNULL,
+                            timeout=3
+                        ).decode(errors='ignore')
+
+                        for line in output.splitlines():
+                            if '<00>' in line and 'GROUP' not in line:
+                                hostname = line.split()[0]
+                                if hostname:
+                                    return hostname
+                    except Exception:
+                        pass
+        try:
+            hostname, _, _ = socket.gethostbyaddr(target_ip)
+            return hostname
+        except Exception:
+            return "Unknown"
