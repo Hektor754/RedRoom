ddd2d0bede2b310bf9b2c90e54a9c748f0cb2954
ektoras <ekal2004@gmail.com>
2025-06-07 20:59:37 +0300

    created

---
+++ categories/recon/methods/vendor_lookup.py
@@ -0,0 +1,18 @@
+def load_oui(filename):
+    oui_map = {}
+    with open(filename, "r") as f:
+        for line in f:
+            if "(hex)" in line:
+                parts = line.strip().split()
+                if len(parts) >= 3:
+                    key = parts[0].replace("-", ":").lower()
+                    vendor = " ".join(parts[2:])
+                    oui_map[key] = vendor
+    return oui_map      
+
+def lookup_vendor(mac, oui_map):
+    if not mac:
+        return "Unknown"
+    
+    mac_prefix = mac.lower()[0:8]
+    return oui_map.get(mac_prefix, "Unknown")   
