7333161d54e422d6ad9c218e81c85861fe0e2638
ektoras <ekal2004@gmail.com>
2025-06-09 22:04:38 +0300

    added logic for port/services

--- categories/recon/hostprofile.py
+++ categories/recon/hostprofile.py
@@ -2,6 +2,22 @@ import ipaddress
 from .methods.auto_host import auto_hostdiscovery
 from .methods.vendor_lookup import load_oui, lookup_vendor
 from utils import print_hostprofile_results
+from .methods.port_service_guess import quick_tcp_scan
+
+class TCPFlags:
+    def __init__(self, ports=None, stealth=False):
+        self.port = ports
+        self.stealth = stealth
+
+common_ports = [20, 21, 22, 23, 25, 53, 67, 68, 69, 80, 110, 111, 119, 123, 135, 137, 138, 
+                139, 143, 161, 162, 179, 194, 389, 443, 445, 465, 514, 515, 520, 587, 631, 
+                636, 993, 995, 1080, 1433, 1434, 1521, 1701, 1812, 1813, 2049, 2082, 2083, 
+                2100, 2483, 2484, 3306, 3389, 3690, 4000, 4444, 5000, 5060, 5432, 5900, 5985, 
+                5986, 6379, 6667, 8000, 8080, 8443, 8888, 9000, 9200, 9300, 11211, 27017, 50000]
+
+efficient_ports = [22, 80, 443, 53, 139, 445, 3306, 8080, 21, 25]
+
+extra_tcp_flags = TCPFlags(ports=efficient_ports, stealth=False)
 
 def run(args):
     if args.method:
@@ -15,11 +31,14 @@ def run(args):
         return
     
     args.silent = True
-    active_hosts = auto_hostdiscovery(args.range, args.timeout, args.retries, args.output, args.format, args.silent, extra_tcp_flags=None)
+    active_hosts = auto_hostdiscovery(args.range, args.timeout, args.retries, args.output, args.format, args.silent, extra_tcp_flags)
     oui_map = load_oui("oui.txt")
     for host in active_hosts:
         mac = host.get("mac", "").strip().lower()
         if mac and mac != "unknown":
             vendor = lookup_vendor(mac, oui_map)
             host["vendor"] = vendor
+        ports, services = quick_tcp_scan(host["ip"],common_ports, args.timeout)
+        host["ports"] = ports
+        host["services"] = services
     print_hostprofile_results(active_hosts)
