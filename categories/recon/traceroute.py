fbef1c0d31731a5049ff75c47ef31c3a6a1f88b2
Hektro754 <ekal2004@gmail.com>
2025-06-27 23:30:51 +0300

    Update categories/recon/traceroute.py

--- categories/recon/traceroute.py
+++ categories/recon/traceroute.py
@@ -1,9 +1,14 @@
 from .methods.protocol_scans.icmp_scan import ICMPtracert
 from .methods.protocol_scans.udp_scan import UDPtracert
+from .methods.protocol_scans.tcp_scan import Handler
 
 def run(args):
     if args.method == "icmp":
         ICMPtracert.icmp_trace(args.range, args.timeout, args.retries, args.output, args.format, args.silent)
     elif args.method == "udp":
         UDPtracert.udp_trace(args.range, args.timeout, args.retries, args.output, args.format, args.silent)
+    elif args.method == "tcp":
+        method = Handler.parse_tcp_flags(args.extra)
+        Handler.tcp_scan(args.range, method, args.timeout, args.retries, args.output, args.format)
+        
         
