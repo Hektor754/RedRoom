8fb223b13bc9a05508032d810fc04f4eff6d1f27
Hektro754 <ekal2004@gmail.com>
2025-06-30 23:15:45 +0300

    Update categories/recon/subenum.py

---
+++ categories/recon/subenum.py
@@ -0,0 +1,53 @@
+import argparse
+from .methods.dns_resolve.subdomain_resolve import Subdomain_Lookup
+
+
+def parse_dns_flags(extra_args):
+    parser_subenum = argparse.ArgumentParser(prog="subenum", add_help=False)
+    parser_subenum.add_argument('--securitytrails-key', type=str, help='API key for SecurityTrails')
+    parser_subenum.add_argument('--virustotal-key', type=str, help='API key for VirusTotal')
+    parser_subenum.add_argument('--certspotter-key', type=str, help='API key for Certspotter')
+    parser_subenum.add_argument('--alienvault-key', type=str, help='API key for AlienVault')
+      
+    args, unknown = parser_subenum.parse_known_args(extra_args)
+    
+    args.extra = unknown 
+    
+    if unknown:
+        print(f"[!] Warning: Unknown Subdomain enumeration options ignored: {unknown}")
+        
+    return args
+
+def run(args):
+    dns_flags = parse_dns_flags(args.extra)
+        
+    sources_to_use = []
+
+
+    if not args.domain:
+        print("[!] Error: No domain specified")
+        return
+
+    domain = args.domain
+    method = args.method
+    
+    if method == "passive":
+        sources_to_use.extend(['crtsh', 'threatcrowd'])
+            
+        if dns_flags.securitytrails_key:
+            sources_to_use.append('securitytrails')
+        if dns_flags.virustotal_key:
+            sources_to_use.append('virustotal')  
+        if dns_flags.certspotter_key:
+            sources_to_use.append('certspotter') 
+
+        api_keys = {
+            k: v for k, v in {
+                'securitytrails': dns_flags.securitytrails_key,
+                'virustotal': dns_flags.virustotal_key,
+                'certspotter': dns_flags.certspotter_key,
+                'alienvault': dns_flags.alienvault_key
+            }.items() if v is not None
+        }
+        
+        Subdomain_Lookup.run(sources_to_use,domain, api_keys=api_keys) 
