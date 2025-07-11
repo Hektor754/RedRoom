743ac898d8b6a09f72fbc3ef77ca21d24874a0cb
Hektro754 <ekal2004@gmail.com>
2025-07-11 23:16:26 +0300

    Update categories/analysis/cvelookup.py

--- categories/analysis/cvelookup.py
+++ categories/analysis/cvelookup.py
@@ -4,7 +4,13 @@ from utils import print_cve_matches,handle_scan_output
 
 def run(args):
     scantype = "cvelookup"
-    tcp_flags = PortScan.parse_tcp_flags(args.extra)
+    class DummyFlags:
+        stealth = False
+        fin = False
+        ack = False
+        xmas = False
+        aggressive = False
+    tcp_flags = DummyFlags()
     results = PortScan.Scan_method_handler(args.range, tcp_flags, args.timeout, args.retries)
     if results:
         matches = CVE_Scan.scan_handler(results)
