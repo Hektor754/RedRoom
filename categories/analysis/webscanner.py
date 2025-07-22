8d5675fb08be769f07215bb0970b2605d966b1d9
ektoras <ekal2004@gmail.com>
2025-07-23 00:44:55 +0300

    Update categories/analysis/webscanner.py

--- categories/analysis/webscanner.py
+++ categories/analysis/webscanner.py
@@ -1,10 +1,10 @@
 from categories.analysis.methods_analysis import (
+    form_analyzer as form_analyzer,
     web_crawler as webcrawler,
-    form_analyser as form_analyser,
     xss_sql_fuzzer as sql_fuzzer,
     tech_detector as tech_detection
 )
-from utils import print_crawl_results,handle_scan_output,print_form_results
+from utils import print_crawl_results,handle_scan_output,print_form_results,print_sql_fuzzer_results
 
 valid_methods = ['wcrawl', 'form', 'sqlfuzz', 'techd', 'all']
 
@@ -36,13 +36,14 @@ def run(args):
         if not args.retries:
             args.retries = 3
         print("[*] Running Form Analyzer...")
-        results['forms'] = form_analyser.run(url, args.file, args.timeout, args.retries)
+        results['forms'] = form_analyzer.run(url, args.file, args.timeout, args.retries)
         print_form_results(results['forms'])
         handle_scan_output(results['forms'], scantype="formanalyser", filename=args.output, ftype=args.format)
     if args.method in ['sqlfuzz', 'all']:
         print("[*] Running SQL/XSS Fuzzer...")
         results['fuzz'] = sql_fuzzer.run(url)
-
+        print_sql_fuzzer_results(results['fuzz'])
+        handle_scan_output(results['fuzz'], scantype="sqlfuzzer", filename=args.output, ftype=args.format)
     if args.method in ['techd', 'all']:
         print("[*] Running Technology Detection...")
         results['tech'] = tech_detection.run(url)
